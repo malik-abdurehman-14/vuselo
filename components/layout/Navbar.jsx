ed7ea8d75a8602569c61a6640dc8caeffc737108
hamza khan <hamzakhan.4u99@gmail.com>
2026-08-25 23:46:48 +0500

    Today first commit

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { Search, ShoppingBag, UserRoundArrowLeft } from "lucide-react";
 
 function Navbar() {
@@ -11,43 +12,51 @@ function Navbar() {
     { name: "Offers", link: "/" },
     { name: "Support", link: "/" },
   ];
+
   return (
-    <div className="flex items-center justify-between py-8 px-20 gap-4 bg-black">
-      <div>
-        <div>
+    <nav className="fixed top-0 left-0 z-50 w-full">
+      <div className="flex items-center justify-between gap-4 px-20 py-5 bg-black/80 backdrop-blur-xl">
+        {/* Logo */}
+        <Link href="/">
           <Image
             src="/logo.png"
             alt="VUSELO"
             width={150}
             height={60}
             priority
-            className="h-auto "
+            className="h-auto"
           />
+        </Link>
+
+        {/* Navigation */}
+        <div className="flex items-center gap-10">
+          {navLinks.map((item, index) => (
+            <Link
+              href={item.link}
+              key={index}
+              className="text-base text-white transition-colors hover:text-gray-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+
+        {/* Icons */}
+        <div className="flex items-center gap-5 text-white">
+          <button className="cursor-pointer transition-opacity hover:opacity-70">
+            <Search size={21} />
+          </button>
+
+          <button className="cursor-pointer transition-opacity hover:opacity-70">
+            <ShoppingBag size={21} />
+          </button>
+
+          <button className="cursor-pointer transition-opacity hover:opacity-70">
+            <UserRoundArrowLeft size={21} />
+          </button>
         </div>
       </div>
-      <div className="flex items-center gap-10">
-        {navLinks.map((item, index) => (
-          <a
-            href={item.link}
-            key={index}
-            className="text-base text-white hover:text-froozen"
-          >
-            {item.name}
-          </a>
-        ))}
-      </div>
-      <div className="flex items-center gap-4 text-white">
-        <div className="cursor-pointer">
-          <Search />
-        </div>
-        <div className="cursor-pointer">
-          <ShoppingBag />
-        </div>
-        <div className="cursor-pointer">
-          <UserRoundArrowLeft />
-        </div>
-      </div>
-    </div>
+    </nav>
   );
 }
 
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -6,15 +6,15 @@ import { Button } from "@/components/ui/Button";
 
 function Hero() {
   return (
-    <section className="relative flex h-screen items-center overflow-hidden bg-black px-20 py-10 text-white">
+    <section className="relative flex h-screen items-center overflow-hidden bg-black py-10 text-white">
       {/* Background Image */}
       <Image src="/hero.png" alt="" fill priority className="object-cover" />
 
       {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black/40" />
+      <div className="absolute inset-0 bg-black/20" />
 
       {/* Content */}
-      <div className="relative z-10 flex max-w-xl flex-col gap-6">
+      <div className="relative z-10 flex max-w-xl flex-col gap-6 px-20">
         <p className="text-base text-froozen">THE NEW STANDARDS IN NICOTINE</p>
 
         <h1 className="text-6xl font-extrabold">One Brand.</h1>
@@ -27,6 +27,25 @@ function Hero() {
 
         <div>
           <Button title="SHOP ALL" variant="dark" textColor="text-froozen" />
+        </div>
+      </div>
+      {/* bottom-content */}
+      <div className="absolute bottom-0 z-10 w-full flex items-center justify-between gap-10 bg-black/85 backdrop-blur-3xl py-14 px-20">
+        <div className="flex flex-col gap-2">
+          <p className="text-froozen text-base">YOUR FIRST MOVE</p>
+          <p className="text-white text-xl">Try The Full System.</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-froozen text-base">
+            OUR E-LIQUIDS CONTAIN NO ADDED SUCRALOSE.
+          </p>
+          <p className="text-white text-xl">High Quality Flavours</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-froozen text-base">
+            NEW TO VUSELO? GET YOUR FREE SAMPLE.
+          </p>
+          <p className="text-white text-xl">Try A Free Can</p>
         </div>
       </div>
     </section>
